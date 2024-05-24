393b6fd4c34a4a64b49fecffd28ea9d97ac65610
shahzeb-inbox <shahzaibimtiazsatti@gmail.com>
2024-05-24 12:06:49 +0200

    committing apache ariflow dags

---
+++ data_engineering/dags/kafka_stream.py
@@ -0,0 +1,65 @@
+import time
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'shahzeb',
+    'start_date': datetime(2024, 5, 22, 10, 00)
+}
+def get_data():
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number']) } {location['street']['name']} {location['city']}, {location['state']}, {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+    return res
+def stream_data():
+    import json
+    import logging
+    from kafka import KafkaProducer
+
+
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+
+    # print(json.dumps(res, indent=3))
+    curr_time = time.time()
+    while True:
+        if time.time() > curr_time + 60: #1 min
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send('user_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
+with DAG('user_automation',
+         default_args = default_args,
+         schedule_interval= '@daily',
+         catchup= False) as dag:
+
+    stream_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable= stream_data
+
+    )
+
